11f08342f360184f6a654848c7d4323777fb866e
Dangerdrive <felippealencar@gmail.com>
2025-03-06 02:47:09 -0300

    03_ongoing

---
+++ Module01/ex03/includes/Weapon.hpp
@@ -0,0 +1,20 @@
+#ifndef WEAPON_H
+# define WEAPON_H
+
+#include <string>
+
+class Weapon {
+private:
+    std::string type;
+
+public:
+    Weapon();
+    Weapon(const std::string& name);
+    ~Weapon();
+
+
+    std::string& getType() const;
+    void setType(const std::string& newType) ;
+};
+
+#endif
