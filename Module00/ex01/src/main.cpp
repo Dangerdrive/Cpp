880953e68167974003c07f2f7ed8b6412eed7b30
Dangerdrive <felippealencar@gmail.com>
2025-03-07 20:32:45 -0300

    - Commit on 2025.03.07\t20h32m45

--- Module00/ex01/src/main.cpp
+++ Module00/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fde-alen <fde-alen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 21:18:11 by marvin            #+#    #+#             */
-/*   Updated: 2025/03/07 20:14:37 by fde-alen         ###   ########.fr       */
+/*   Updated: 2025/03/07 20:22:56 by fde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,43 @@
 #include "../includes/PhoneBook.hpp"
 
 int main() {
-  Phonebook phonebook;
-  Contact new_contact;
-  std::string command;
+	Phonebook phonebook;
+	Contact new_contact;
+	std::string command;
 
-  std::cout << "Welcome to PhoneBook" << std::endl;
+	std::cout << "Welcome to PhoneBook" << std::endl;
 
-  while (1) {
-    std::cout << "Enter command: ";
-    std::getline(std::cin,
-                 command);
-    if (command.empty()) {
-    std::cout << "Error: No command entered. Please try again." << std::endl;
-    continue;
-    }
-    if (command.compare("ADD") == 0) {
-      new_contact.setField("First Name");
-      new_contact.setField("Last Name");
-      new_contact.setField("Nickname");
-      new_contact.setField("Phone Number");
-      new_contact.setField("Darkest Secret");
-      phonebook.AddContact(new_contact);
-    } else if (command.compare("SEARCH") == 0) {
-      if (phonebook.getContactCount() == 0) {
-        std::cout << "No contacts added. ADD a contact first." << std::endl;
-      } else {
-        phonebook.DisplaySearchContacts();
-        phonebook.Search();
-      }
-      continue;
-    } else if (command.compare("EXIT") == 0) {
-      std::cout << "Exiting PhoneBook..." << std::endl;
-      return 0;
-      
-    } else 
-                std::cout << "Invalid command. Try a valid one: ADD, SEARCH or EXIT"
-                << std::endl;
-      }
-    }
+	while (1) {
+		std::cout << "Enter command: ";
+		std::getline(std::cin, command);
+
+		if (command.empty()) {
+			std::cout << "Error: No command entered. Please try again."
+					  << std::endl;
+			continue;
+		}
+		if (command.compare("ADD") == 0) {
+			new_contact.setField("First Name");
+			new_contact.setField("Last Name");
+			new_contact.setField("Nickname");
+			new_contact.setField("Phone Number");
+			new_contact.setField("Darkest Secret");
+			phonebook.AddContact(new_contact);
+		} else if (command.compare("SEARCH") == 0) {
+			if (phonebook.getContactCount() == 0) {
+				std::cout << "No contacts added. ADD a contact first."
+						  << std::endl;
+			} else {
+				phonebook.DisplaySearchContacts();
+				phonebook.Search();
+			}
+			continue;
+		} else if (command.compare("EXIT") == 0) {
+			std::cout << "Exiting PhoneBook..." << std::endl;
+			return (0);
+		} else {
+			std::cout << "Invalid command. Try a valid one: ADD, SEARCH or EXIT"
+					  << std::endl;
+		}
+	}
+}
