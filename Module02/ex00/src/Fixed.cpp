28203f8fd5c51a2d578ca876f1e8fa6613e2ef05
Dangerdrive <felippealencar@gmail.com>
2025-03-21 21:42:27 -0300

    Commit on 2025-03-21 21:42:27

--- Module02/ex00/src/Fixed.cpp
+++ Module02/ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: fde-alen <fde-alen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 17:10:09 by fde-alen          #+#    #+#             */
-/*   Updated: 2025/03/16 23:25:36 by fde-alen         ###   ########.fr       */
+/*   Updated: 2025/03/21 21:34:27 by fde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,8 @@ Fixed::Fixed(const Fixed& other) {
 
 Fixed& Fixed::operator=(const Fixed& other) {
     std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &other) 
+    if (this != &other)
         this->_value = other.getRawBits();
-    }
     return *this;
 }
 
